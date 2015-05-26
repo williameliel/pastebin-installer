56b1f75f88e4df05a16cfc8a13b1034313dc14c0
E-Ratic Madman <william.garcia@wearesideways.com>
2015-05-26 13:00:23 -0400

    Installer

---
+++ pastebin.sh
@@ -0,0 +1,20 @@
+#Pastebin WP automated installation process, installs Wordpress with custom directory
+#Get latest Wordpress
+wget http://wordpress.org/latest.tar.gz && tar xfz latest.tar.gz && rm -f latest.tar.gz
+#Create custom /wp directory structure
+mv wordpress wp
+cp -r wp/wp-content content
+cd content/themes 
+#Copy pastebin theme
+git clone https://swnyc.git.beanstalkapp.com/pastebin-2.git pastebin 
+cd pastebin 
+# get custom wp-config and htaccess & other files to wp root
+mv core/index.php  ../../../
+mv wp-config.php ../../../
+mv local-config.php ../../../
+mv .htaccess ../../../
+mv manifest.webapp ../../../
+mv *.txt ../../../
+mv crossdomain.xml ../../../
+rm -rf .git* 
+echo "-*- Done -*-"
